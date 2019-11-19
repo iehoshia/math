9887b116582d1da865f8ea22ea0124e3957aaf59
iehoshia <jepgez@gmail.com>
2019-11-19 10:02:14 -0600

    Update rectangle.py

--- rectangle.py
+++ rectangle.py
@@ -3,11 +3,11 @@ def calculate_area(sidea, sideb):
 	return sidea * sideb
 
 def calculate_perimeter(sidea, sideb):
-	return 2 * sidea + 2 sideb
+	return 2 * sidea + 2 * sideb
 
 if __name__ == "__main__":
 	sidea = int(input('Enter side A: '))
-	sideb = int(input('Enter side B'))
+	sideb = int(input('Enter side B: '))
 
 	area = calculate_area(sidea, sideb)
 	perimeter = calculate_perimeter(sidea, sideb)
