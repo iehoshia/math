a270b8bf3c3d2534f319d77b023fab4dc33133bd
iehoshia <jepgez@gmail.com>
2019-11-21 09:42:03 -0600

    Update pyqt-1.py

--- pyqt-1.py
+++ pyqt-1.py
@@ -4,7 +4,8 @@ print ("This is the name of the script: ", sys.argv[0])
 print ("Number of arguments: ", len(sys.argv))
 print ("The arguments are: " , str(sys.argv))
 
-from PyQt5.QtWidgets import QApplication, QLabel, QWidget
+from PyQt5.QtWidgets import QApplication, \
+	QLabel, QWidget
 
 if __name__ == '__main__':
 	app = QApplication(sys.argv)
