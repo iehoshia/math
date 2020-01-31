e546faaa94cc9d53081c916c0a217428eae9e504
iehoshia <jepgez@gmail.com>
2020-01-31 10:23:55 -0600

    Cycle for

--- test31012020/app.py
+++ test31012020/app.py
@@ -16,5 +16,11 @@ def phone():
    return render_template('phone.html',
    	phone='5550000')
 
+@app.route('/for')
+def cyclefor():
+	datas = [1,2,3,4,5,6,9,10]
+    return render_template('ciclofor.html',
+   	datas=datas)
+
 if __name__ == '__main__':
    app.run(port=8002, host='0.0.0.0', debug=True)
