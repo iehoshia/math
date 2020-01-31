93100855c9408230e7cff427079ea1f5388384de
iehoshia <jepgez@gmail.com>
2020-01-31 10:41:20 -0600

    Update app.py

--- test31012020/app.py
+++ test31012020/app.py
@@ -4,23 +4,23 @@ app = Flask(__name__)
 @app.route('/')
 def index():
    return render_template('hello.html',
-   	name='JOSIAS')
+    name='JOSIAS')
 
 @app.route('/city')
 def city():
    return render_template('city.html',
-   	city='Xela')
+    city='Xela')
 
 @app.route('/phone')
 def phone():
-   return render_template('phone.html',
-   	phone='5550000')
+   return render_template('phones.html',
+    phone='5550000')
 
 @app.route('/for')
 def cyclefor():
-	datas = [1,2,3,4,5,6,9,10]
+    datas = [1,2,3,4,5,6,9,10]
     return render_template('ciclofor.html',
-   	datas=datas)
+        datas=datas)
 
 if __name__ == '__main__':
    app.run(port=8002, host='0.0.0.0', debug=True)
