7c070c8ff8a654099e67e5614004745a5fabcc04
iehoshia <jepgez@gmail.com>
2020-02-16 13:09:45 -0600

    Update rsa.py

--- test16022020/rsa.py
+++ test16022020/rsa.py
@@ -71,3 +71,81 @@ def is_prime(num):
     return True
 
 print("127",is_prime(127))
+
+def generate_keypair(p, q):
+    if not (is_prime(p) and is_prime(q)):
+        raise ValueError("Both numbers must be prime.")
+    elif p == q:
+        raise ValueError('p and q cannot be equal')
+    n = p * q
+
+    #Phi is the totient of n
+    phi = (p-1) * (q-1)
+
+    #Choose an integer e such that e and phi(n) are coprime
+    e = random.randrange(1, phi)
+
+    #Use Euclid's Algorithm to verify that e and phi(n) are comprime
+    g = gcd(e, phi)
+    while g != 1:
+        e = random.randrange(1, phi)
+        g = gcd(e, phi)
+
+    #Use Extended Euclid's Algorithm to generate the private key
+    d = multiplicative_inverse(e, phi)
+
+    #Return public and private keypair
+    #Public key is (e, n) and private key is (d, n)
+    return ((e, n), (d, n))
+
+print(generate_keypair(19,29))
+
+def encrypt(pk, plaintext):
+    #Unpack the key into it's components
+    key, n = pk
+    #Convert each letter in the plaintext to numbers based on the character using a^b mod m
+    #cipher = [(ord(char) ** key) % n for char in plaintext]
+    chipher = [pow(ord(char),key,n) for char in plaintext]
+    #Return the array of bytes
+    return chipher
+
+print(encrypt((257,551),'JOSIAS'))
+print(encrypt((257,551),'FERNANDO'))
+
+def decrypt(pk, ciphertext):
+    #Unpack the key into its components
+    key, n = pk
+    #Generate the plaintext based on the ciphertext and key using a^b mod m
+    #plain = [chr((char ** key) % n) for char in ciphertext]
+    plain = [chr(pow(char, key, n)) for char in ciphertext]
+    #Return the array of bytes as a string
+    return ''.join(plain)
+
+
+if __name__ == '__main__':
+    '''
+    Detect if the script is being run directly by the user
+    '''
+    print("RSA Encrypter/ Decrypter")
+    prime = False
+    while prime == False:
+        p = int(input("Enter a prime number (17, 19, 23, etc): "))
+        prime = is_prime(p)
+    prime = False
+    while prime == False:
+        q = int(input("Enter another prime number (Not one you entered above): "))
+        if p == q:
+            print("p can not be equal to q")
+            prime =False
+            continue
+        prime = is_prime(q)
+    print ("Generating your public/private keypairs now . . .")
+    public, private = generate_keypair(p, q)
+    print ("Your public key is ", public ," and your private key is ", private)
+    message = input("Enter a message to encrypt with your private key: ")
+    encrypted_msg = encrypt(private, message)
+    print ("Your encrypted message is: ")
+    print (''.join(map(lambda x: str(x), encrypted_msg)))
+    print ("Decrypting message with public key ", public ," . . .")
+    print ("Your message is:")
+    print (decrypt(public, encrypted_msg))
